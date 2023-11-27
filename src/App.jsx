405a9b185a3053c5c87bd64c2f323713570a969d
nipunchamara98 <139055329+nipunchamara98@users.noreply.github.com>
2023-11-27 18:22:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const a=60
+  
+  return (
+    <>
+      <h1 className="heading">Nipun</h1>
+      <p>abcd</p>
+      <p>{10*10}% for jsx</p>
+      <p>{a}</p>
+      <p>{a>50?"Greater" :"Lesser"}</p>
+    </>
+  )
+}
+
+export default App
